81f98935c7c820f55787b73309fb0eae689cbd69
FlowJune <flow996625@gmail.com>
2017-05-17 13:17:38 +0800

    0517
    changebk & so on

---
+++ Assets/UI/loading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour {
+    public int e;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(delegate
+        {
+            ClickBtn(this.gameObject);
+        });
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void ClickBtn(GameObject obj)
+    {
+        Application.LoadLevel(e); Debug.Log("fuck");
+        Debug.Log("ClickBtn");
+    }
+}
